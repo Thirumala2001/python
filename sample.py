9493f39b6c5ca0441aee4943ed883855adae6e0d
Thirumala2001 <yarramneedithirumala@gmail.com>
2024-07-15 16:55:58 +0530

    mn

---
+++ sample.py
@@ -0,0 +1,15 @@
+a=10
+list = [2,'thirumala']
+dict = {'name':'thirumala','id':1,'list':[5,6]}
+str="ghjfkd"
+print(list[1])
+print(dict['list']);
+print(str[0:2])
+print(str[:-1])
+print(str[2:])
+print(type(a))
+
+
+
+
+
