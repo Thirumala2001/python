0dc416f202df9b523640076edf4797f252b5f65c
Thirumala2001 <yarramneedithirumala@gmail.com>
2024-07-16 11:02:50 +0530

    updated

---
+++ ds.py
@@ -0,0 +1,28 @@
+#list is nonhomogeneous and mutable
+#list is ordered datastructure
+'''list=[12,"e",8.9,0.000000]
+list.append(20)
+list.insert(1,100) #insert a value at particular index
+list.pop(1) # Delete element by index
+list.remove(8.9) # delete directly by element
+print(list)
+'''
+#tuple is nonhomogeneous and immutable
+#tuple is ordered datastructure
+'''
+tuple=(12,13.14,15,"jhjk")
+#tuple.append(20)
+print(tuple)
+'''
+# set nonhomogeneous and unordered ds
+set = {23,56,"wee",8.0}
+type(set)
+set.add("fdsas")
+set.add(90)
+set.remove(23)
+print(set)
+
+dict = {"a":10,"b":20,"c":30}
+print(dict)
+
+
---
+++ functions.py
@@ -0,0 +1,9 @@
+'''
+def method(first,last):
+    name=first+" "+last
+    return name
+firstname = input("enter first name")
+lastname = input(" enter last name")
+fullname = method(firstname,lastname)
+print (fullname)
+'''
